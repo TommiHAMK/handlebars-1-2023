f75776cae69676d02a45a9d1165060ecc6e747a2
aatosmedia <tommi.saksa@aatosmedia.fi>
2023-04-14 11:45:30 +0300

    Added loop for products

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+// Dummy "database"
+let products = [   
+    { id: 1, name: 'Chair', price: 100 },   
+    { id: 2, name: 'Table', price: 200 }   
+];
+
+
 app.engine('handlebars', exphbs.engine({
     defaultLayout: 'main'
 }));
@@ -23,6 +30,17 @@ app.get('/about-us', (req, res) => {
     res.render('about-us',
     {
         pagetitle: "Our Story"
+    });
+})
+
+
+
+app.get('/products', (req, res) => {
+    res.render('products',
+    {
+        pagetitle: "Our Products",
+        desc: "Take a look at our cool products!",
+        products : products
     });
 })
 
