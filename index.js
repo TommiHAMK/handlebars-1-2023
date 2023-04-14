627da4d22dbee90f94aff3507330dbf802b5bf66
aatosmedia <tommi.saksa@aatosmedia.fi>
2023-04-14 11:17:25 +0300

    Render some views - no data used yet

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', exphbs.engine({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+
+
 app.get('/', (req, res) => {
-    res.send("Testing");
+    //res.send("Testing");
+    res.render('index');
 })
+
+app.get('/about-us', (req, res) => {
+    res.render('about-us');
+})
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening port ${PORT}`));
-
